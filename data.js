760712f2aa22b50851f19826faa2efede7a722b2
cato1412-dev <buichithanh5198@gmail.com>
2025-07-13 13:16:53 +0700

    main

---
+++ data.js
@@ -0,0 +1,55 @@
+const donateData = [
+  {
+    device: "Oneplus Ace 3v",
+    rom: "OxygenOS",
+    version: "All",
+    image: "/img/trung.png",
+    title: "Thông tin Donate",
+    description: `
+      Ngân hàng: 108868096941 | TRAN DUC TRUNG | VIETINBANK<br>
+      Paypal: <a href="https://www.paypal.com/paypalme/maihcx" target="_blank" class="text-blue-500 underline">NHẤN VÀO ĐÂY</a><br>
+      Sau khi quyên góp vui lòng liên hệ: <a href="https://t.me/trungtran23293" target="_blank" class="text-blue-500 underline">Trung Trần</a> 
+      hoặc <a href="https://t.me/maiphp" target="_blank" class="text-blue-500 underline">Mai || AFK ||</a> 
+      hoặc <a href="https://t.me/VieOS_discussions" target="_blank" class="text-blue-500 underline">Nhóm hỗ trợ</a> .<br>
+    `
+  },
+  {
+    device: "Oneplus Ace 3v",
+    rom: "ColorOS",
+    version: "All",
+    image: "/img/trung.png",
+    title: "Thông tin Donate",
+    description: `
+      Ngân hàng: 108868096941 | TRAN DUC TRUNG | VIETINBANK<br>
+      Paypal: <a href="https://www.paypal.com/paypalme/maihcx" target="_blank" class="text-blue-500 underline">NHẤN VÀO ĐÂY</a><br>
+      Sau khi quyên góp vui lòng liên hệ: <a href="https://t.me/trungtran23293" target="_blank" class="text-blue-500 underline">Trung Trần</a> 
+      hoặc <a href="https://t.me/maiphp" target="_blank" class="text-blue-500 underline">Mai || AFK ||</a> 
+      hoặc <a href="https://t.me/VieOS_discussions" target="_blank" class="text-blue-500 underline">Nhóm hỗ trợ</a> .<br>
+    `
+  },
+  {
+    device: "Oneplus Ace 5",
+    rom: "OxygenOS",
+    version: "v1 - v8",
+    image: "/img/trung.png",
+    title: "Thông tin Donate",
+    description: `
+      Ngân hàng: 108868096941 | TRAN DUC TRUNG | VIETINBANK<br>
+      Paypal: <a href="https://www.paypal.com/paypalme/maihcx" target="_blank" class="text-blue-500 underline">NHẤN VÀO ĐÂY</a><br>
+      Sau khi quyên góp vui lòng liên hệ: <a href="https://t.me/trungtran23293" target="_blank" class="text-blue-500 underline">Trung Trần</a> 
+      hoặc <a href="https://t.me/maiphp" target="_blank" class="text-blue-500 underline">Mai || AFK ||</a> 
+      hoặc <a href="https://t.me/VieOS_discussions" target="_blank" class="text-blue-500 underline">Nhóm hỗ trợ</a> .<br>
+    `
+  },
+  {
+    device: "Oneplus Ace 5",
+    rom: "ColorOS",
+    version: "v1",
+    image: "/img/moc.png",
+    title: "Thông tin Donate",
+    description: `
+      Sau khi quyên góp vui lòng liên hệ: <a href="https://t.me/jerryx0x" target="_blank" class="text-blue-500 underline">Mốc</a> 
+      hoặc <a href="https://t.me/VieOS_discussions" target="_blank" class="text-blue-500 underline">Nhóm hỗ trợ</a> .<br>
+    `
+  }
+];
