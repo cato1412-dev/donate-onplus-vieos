c7df9dc48da9700886143362c608c97d3bfc367c
cato1412-dev <buichithanh5198@gmail.com>
2025-07-21 17:59:19 +0700

    Update data.js

--- data.js
+++ data.js
@@ -43,6 +43,17 @@ const donateData = [
   },
   {
     device: "Oneplus Ace 5",
+    rom: "OxygenOS",
+    version: "v9",
+    image: "/donate-onplus-vieos/img/moc.png",
+    title: "Thông tin Donate",
+    description: `
+      Sau khi quyên góp vui lòng liên hệ: <a href="https://t.me/jerryx0x" target="_blank" class="text-blue-500 underline">Mốc</a> 
+      hoặc <a href="https://t.me/VieOS_discussions" target="_blank" class="text-blue-500 underline">Nhóm hỗ trợ</a> .<br>
+    `
+  },
+  {
+    device: "Oneplus Ace 5",
     rom: "ColorOS",
     version: "v1",
     image: "/donate-onplus-vieos/img/moc.png",
